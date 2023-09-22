60fd706584bd26f6be8bceee46c6bd73c72968ab
Yoga Raditya <radityanalaa@gmail.com>
2023-09-22 10:58:02 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import time, os
 
-timer_input = input("Enter time in minute : ")
+timer_input = input("Enter time in (hh:mm:ss) format : ")
 timer = timer_input.split(":")
 hour = timer[0]
 minute = timer[1]
 second = timer[2]
 
-for hours in range (int(hour), -1, -1):
-    for mins in range (int(minute), -1, -1):
-        for sec in range (int(second), -1, -1):
+for hours in range(int(hour), -1, -1):
+    for mins in range(int(minute), -1, -1):
+        for sec in range(int(second), -1, -1):
             os.system('cls')
             time_remaining = f"{hours} jam {mins} menit {sec} detik"
             print("+" + "-" * 30 + "+")
@@ -25,5 +25,5 @@ for hours in range (int(hour), -1, -1):
     minute = 59
 
 
-print(" "*100, end='\r')
-print("waktu hbis bg, turu")
+os.system('cls')
+print("Time out!")
